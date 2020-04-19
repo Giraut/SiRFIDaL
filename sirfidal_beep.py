c14019d9d90fe4781b403e0f1b9c49136c17ccfb
giraut <pierre.coupard@cinemanext.com>
2020-04-20 00:45:22 +0300

    Replaced playsound with pyaudio, because there is a deb package for pyaudio, so we stick to Python3 packages that can be installed with apt (with dependencies) and we don't have to tell the user to use pip. Also made it possible to disable the up or down sound by passing filename '-' or '' to arguments -u or -d.

--- sirfidal_beep.py
+++ sirfidal_beep.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Script to play a sound when a RFID or NFC transponder is presented to a
-reader, and another when it is taken out.
+"""Script to play a WAV sound file when a RFID or NFC transponder is presented
+to a reader, and another when it is taken out.
 
 This script is a SiRFIDaL client. It effectively adds audio feedback to readers
 that don't have an internal buzzer. This is particularly useful with RFID or
@@ -8,9 +8,12 @@ NFC implants, with which is can sometimes be difficult to find the "sweet spot"
 on certain readers, due to the reduced range of glass implant transponders.
 
 The script asks the server to get notified when UIDs become active or inactive,
-then simply plays the corresponding sound file. The sound files to be played
-may be specified with the -u and -d arguments, or encoded permanently in the
-parameters below.
+then simply plays the corresponding sound file.
+
+The sound files to be played may be specified with the -u and -d arguments, or
+encoded permanently in the parameters below.
+
+if -u or -d is set to '-' or '', no sound is played.
 """
 
 ### Parameters
@@ -24,10 +27,57 @@ socket_path="/tmp/sirfidal_server.socket"
 import re
 import os
 import sys
+import wave
 import argparse
 from time import sleep
-from playsound import playsound
+from pyaudio import PyAudio
 from socket import socket, AF_UNIX, SOCK_STREAM, SOL_SOCKET, SO_PASSCRED
+
+
+
+### Subroutines
+def play_wav_file(fpath):
+
+  # Open the WAV file
+  f=wave.open(fpath, "rb")
+
+  # Redirect stderr to /dev/null to hide useless PortAudio messages
+  devnull=os.open(os.devnull, os.O_WRONLY)
+  old_stderr=os.dup(2)
+  sys.stderr.flush()
+  os.dup2(devnull, 2)
+  os.close(devnull)
+
+  try:
+    p=PyAudio()
+  except:
+    # Restore stderr
+    os.dup2(old_stderr, 2)
+    os.close(old_stderr)
+    raise
+
+  # Restore stderr
+  os.dup2(old_stderr, 2)
+  os.close(old_stderr)
+
+  # Open the stream
+  stream=p.open(
+	  format=p.get_format_from_width(f.getsampwidth()),
+	  channels=f.getnchannels(),
+	  rate=f.getframerate(),
+	  output=True
+	)
+
+  # Send the WAV file to the stream in chunks
+  data=f.readframes(1024)
+  while data:
+    stream.write(data)
+    data=f.readframes(1024)
+
+  stream.stop_stream()
+  stream.close()
+
+  p.terminate()
 
 
 
@@ -41,20 +91,21 @@ def main():
   argparser.add_argument(
 	  "-u", "--upsoundfile",
 	  type=str,
-	  help="Sound file to play when a new UID comes up",
+	  help="WAV sound file to play when a new UID comes up (- to disable)",
           required=False
 	)
   argparser.add_argument(
 	  "-d", "--downsoundfile",
 	  type=str,
-	  help="Sound file to play when a UID goes away",
+	  help="WAV sound file to play when a UID goes away (- to disable)",
           required=False
 	)
   args=argparser.parse_args()
 
-  upsndfile=args.upsoundfile if args.upsoundfile else default_up_sound_file
-  downsndfile=args.downsoundfile if args.downsoundfile \
-		 else default_down_sound_file
+  upsndfile=args.upsoundfile if args.upsoundfile!=None \
+		else default_up_sound_file
+  downsndfile=args.downsoundfile if args.downsoundfile!=None \
+		else default_down_sound_file
 
   sock=None
 
@@ -90,6 +141,9 @@ def main():
     # Get data from the socket
     try:
       b=sock.recv(256).decode("ascii")
+    except KeyboardInterrupt:
+      sock.close()
+      return(0)
     except:
       sock.close()
       sock=None
@@ -124,17 +178,17 @@ def main():
         # increased
         chg=float(m[0][1])
 
-        if chg > 0:
+        if chg > 0 and upsndfile and upsndfile!="-":
           try:
-            playsound(upsndfile)
+            play_wav_file(upsndfile)
           except:
             print("Error: cannot play {} sound file".format(upsndfile))
 
         # Play the "down" sound file if the number of active UIDs has
         # decreased
-        elif chg < 0:
+        elif chg < 0 and downsndfile and downsndfile!="-":
           try:
-            playsound(downsndfile)
+            play_wav_file(downsndfile)
           except:
             print("Error: cannot play {} sound file".format(downsndfile))
 
