062f04123f23c7324777c572114b8c5c3112872b
Giraut <>
2021-12-09 15:53:04 +0200

    Rewrote sirfidal_auto_send_enter_at_login.py so that it always determines whether a session is running on a text or graphical console by getting the list of running sessions fron loginctl and checking if a session is running on the active console. If no session is found (text console) or a "greeter" session is found (graphical login), the keystroke sequence for ENTER is sent to the console using uinput in both cases, instead of using either uinput or synthetic X events. This has the advantage of reducing the complexity due to X library calls, better identifying login screens (particularly graphical ones) than using extended WM attributes, and work with graphical logins that won't accept synthetic X events (e.g. gdm). The main drawback is tying the utility to systemd, because loginctl is part of systemd.

--- sirfidal_auto_send_enter_at_login.py
+++ sirfidal_auto_send_enter_at_login.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Utility to automatically type ENTER in a virtual console (text) or in a
-X display manager login screen (graphical) when SiRFIDaL reads a RFID or NFC
-UID.
+"""Utility to automatically send ENTER to a virtual console when SiRFIDaL reads
+a RFID or NFC UID and no user session is running - i.e. when a text or
+graphical login screen is running on the virtual console.
 
 This is useful to dismiss the regular keyboard password entry and immediately
 move on to the RFID authentication, if both are used in the PAM configuration.
@@ -15,6 +15,10 @@ Example: if the first authentication is pam_unix and the second authentication
          or NFC tag, and the utility types the blank password automatically,
          with sirfidal_pam getting the UID immediately afterward.
 
+This script calls loginctl to determine the active session (or lack thereof)
+on the current virtual console. loginctl is part of systemd. In other words,
+this script will not work on systemd-less distros.
+
 THIS SCRIPT MUST BE RUN AS ROOT!
 """
 
@@ -24,23 +28,14 @@ import os
 import psutil
 from time import sleep
 from evdev import UInput, ecodes
+from subprocess import Popen, PIPE
 import sirfidal_client_class as scc
-
-try:
-  import Xlib.XK
-  from Xlib import X
-  from Xlib.protocol import event
-  from Xlib.display import Display
-  do_x = True
-except:
-  do_x = False
 
 
 
 ### Routines
 def active_vc():
-  """Return the name of the current virtual console or None in case of error.
-  (Linux-specific)
+  """Return the name of the current virtual console or None in case of error
   """
 
   try:
@@ -53,105 +48,66 @@ def active_vc():
 
 
 
-def is_console_session_running(vc):
-  """Returns True if processes other than getty or login are attached to a
-  virtual console, False if getty, login or both are attached but nothing else,
-  and None in case of error
+def get_session_class(vc):
+  """Use loginctl to iterate over the list of currently-running sessions and
+  return the class of any session running on the virtual console vc, "" if no
+  session is found running on that virtual console, or None in case of error
   """
 
-  if not vc:
+  if vc is None:
     return None
 
-  getty_or_login_attached = False
-  non_getty_or_login_attached = False
-
-  for p in psutil.process_iter():
-
-    if p.terminal() == "/dev/" + vc:
-      if p.name() in ("getty", "agetty", "login"):
-        getty_or_login_attached = True
-      else:
-        non_getty_or_login_attached = True
-
-      if getty_or_login_attached and non_getty_or_login_attached:
-        break
-
-  if non_getty_or_login_attached:
-    return True
-
-  if getty_or_login_attached:
-    return False
-
-  return None
-
-
-
-def xorg_attached_to_vc(vc):
-  """Find an Xorg process attached to a virtual console and return the DISPLAY
-  and Xauthority files attached to it. If no Xorg process is attached to the
-  virtual terminal or in case of error, return (None, None)
-  """
-
-  display = None
-  xauthfile = None
-
-  if vc:
-
-    for p in psutil.process_iter():
-      if p.name() == "Xorg" and p.terminal() == "/dev/" + vc:
-
-        next_arg_is_xauthfile = False
-
-        for arg in p.cmdline():
-          if re.match("^:[0-9]+$", arg):
-            display = arg
-
-          elif arg == "-auth":
-            next_arg_is_xauthfile = True
-
-          elif next_arg_is_xauthfile:
-            xauthfile = arg
-            next_arg_is_xauthfile = False
-
-  return (display, xauthfile)
-
-
-
-def open_x_display(display, xauthfile):
-  """Open a connection to an X display. Return the Display object or None in
-  case of failure
-  """
-
-  xauth_orig = os.environ.get("XAUTHORITY")
-  os.environ["XAUTHORITY"] = xauthfile
-
+  # Get the list of sessions
   try:
-    d = Display(display)
-  except:
-    d = None
+    p = Popen(["loginctl", "list-sessions"], stdout = PIPE)
+    loginctl_stdout = p.communicate()[0].decode("utf-8")
+    if p.returncode:
+      return None
 
-  if xauth_orig:
-    os.environ["XAUTHORITY"] = xauth_orig
-  else:
-    del(os.environ["XAUTHORITY"])
-
-  return d
-
-
-
-def is_wm_running(d, r):
-  """ Returns True if an ICCM or EWMH window manager is running as the root
-  window of an X display, False if no windows manager is found and None in case
-  of error.
-  """
-
-  try:
-    return r.get_full_property(d.intern_atom("_NET_SUPPORTING_WM_CHECK"),
-				d.intern_atom("CARDINAL")) is not None or \
-		r.get_full_property(d.intern_atom("_WIN_SUPPORTING_WM_CHECK"),
-				d.intern_atom("CARDINAL")) is not None
   except:
     return None
+    pass
+
+  sessionids = []
+  for l in loginctl_stdout.split("\n"):
+    m = re.findall("^\s*(\S+)\s+([0-9]+)\s+([!-,\.0-9;A-~]{1,32})\s+.*$", l)
+    if m:
+      sessionids.append(m[0][0])
+
+  # For each session, get the associated virtual console and class. If the
+  # virtual console matches, return the class
+  for sessionid in sessionids:
+
+    # Get the session information
+    try:
+      p = Popen(["loginctl", "show-session", sessionid], stdout = PIPE)
+      loginctl_stdout = p.communicate()[0].decode("utf-8")
+      if p.returncode:
+        return None
+
+    except:
+      return None
+      pass
+
+    # Extract the virtual console and class. Skip if the virtual console
+    # doesn't match. Return the class if it does
+    session_vtnr = None
+    session_class = None
+    for l in loginctl_stdout.split("\n"):
+      m = re.findall("^(VTNr|Class)=([\S+]+)$", l)
+      if m:
+        if m[0][0] == "VTNr":
+          session_vtnr = "tty{}".format(m[0][1])
+        else:
+          session_class = m[0][1]
+        if session_vtnr is not None:
+          if session_vtnr != vc:
+            break
+          elif session_class is not None:
+            return session_class
+
+  # We didn't find a session with a matching virtual console number
+  return ""
 
 
 
@@ -193,67 +149,11 @@ def main():
               print("Error determining the active virtual console")
               continue
 
-            # If we do X: is an X server attached to the virtual console?
-            if do_x:
-              display, xauthfile = xorg_attached_to_vc(vc)
+            # If no session is running on the active virtual console, or a
+            # greeter session is running, send the keystroke sequence for
+            # ENTER to the console
+            if get_session_class(vc) in ("", "greeter"):
 
-            if do_x and display is not None and xauthfile is not None:
-
-                # Open the X display
-                d = open_x_display(display, xauthfile)
-                if d is not None:
-
-                  # Get the root window
-                  try:
-                    r = d.screen().root
-                  except:
-                    r = None
-
-                  # Check whether a ICCM or EWMH window manager is running on
-                  # the X server - in which case we can be reasonably sure a
-                  # session is already open. If the check succeeds and no
-                  # window manager is present, send a Return key event. Abstain
-                  # in case of error, or if a window manager is running
-                  if r is not None and is_wm_running(d, r) == False:
-
-                    # Send Return to the X window currently in focus - i.e. the
-                    # display manager, since nothing else should be displayed
-                    try:
-                      enter_keysym = Xlib.XK.string_to_keysym("Return")
-                      enter_keycode = d.keysym_to_keycode(enter_keysym)
-                      f = d.get_input_focus().focus
-                      f.send_event(event.KeyPress(detail = enter_keycode,
-						time = 0,
-						root = r, window = f,
-                                                same_screen = 0,
-						child = X.NONE,
-						root_x = 0, root_y = 0,
-						event_x = 0, event_y = 0,
-						state = 0))
-                      f.send_event(event.KeyRelease(detail = enter_keycode,
-						time = 0,
-						root = r, window = f,
-                                                same_screen = 0,
-						child = X.NONE,
-						root_x = 0, root_y = 0,
-						event_x = 0, event_y = 0,
-						state = 0))
-                      print("Return sent to the X window")
-                    except:
-                      print("Error sending Return keysym to the X window")
-
-                  # Close the X display
-                  try:
-                    d.close()
-                  except:
-                    pass
-
-            # Is getty and/or login attached to the virtual console and no
-            # session open?
-            elif is_console_session_running(vc) == False:
-
-              # Send the keystroke sequence corresponding to ENTER to the
-              # console
               try:
                 ui = UInput()
               except:
@@ -262,8 +162,11 @@ def main():
 
               try:
                 ui.write(ecodes.EV_KEY, ecodes.KEY_ENTER, 1)
+                ui.syn()
+                sleep(.1)   # Pause needed for gdm
                 ui.write(ecodes.EV_KEY, ecodes.KEY_ENTER, 0)
                 ui.syn()
+                sleep(.1)   # Pause needed for gdm
                 print("ENTER sent to console {}".format(vc))
               except:
                 print("UInput write error")
